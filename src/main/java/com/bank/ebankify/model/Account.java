2343940aafe7b239b543410076eb165d3d2f4acf
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-10-29 11:53:35 +0100

    create Acount Entity

---
+++ src/main/java/com/bank/ebankify/model/Account.java
@@ -0,0 +1,39 @@
+package com.bank.ebankify.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "bank_accounts")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "account_number")
+    private String account_number;
+
+    @Column(name = "balance")
+    private Double balance;
+
+    @Column(name = "owner_id")
+    private Long owner_id ;
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", account_number='" + account_number + '\'' +
+                ", balance=" + balance +
+                ", owner_id=" + owner_id +
+                '}';
+    }
+}
