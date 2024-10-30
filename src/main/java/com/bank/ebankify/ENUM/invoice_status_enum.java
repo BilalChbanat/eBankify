1c3cff65ca9e45b12953cdfae900253cb8db5ff9
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-10-30 09:22:36 +0100

    creating invoice with enum status

---
+++ src/main/java/com/bank/ebankify/ENUM/invoice_status_enum.java
@@ -0,0 +1,7 @@
+package com.bank.ebankify.ENUM;
+
+public enum invoice_status_enum {
+    PAID,
+    UNPAID,
+    OVERDUE
+}
---
+++ src/main/java/com/bank/ebankify/model/Invoice.java
@@ -0,0 +1,37 @@
+package com.bank.ebankify.model;
+
+import com.bank.ebankify.ENUM.invoice_status_enum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "invoices")
+public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "amount_due", nullable = false)
+    private Double amountDue;
+
+    @Column(name = "due_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dueDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private invoice_status_enum status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
