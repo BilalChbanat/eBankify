69971f0f2f196a08aa0751d908b8e141d674d16e
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-10-30 12:18:24 +0100

    recreate the project

--- src/main/java/com/bank/ebankify/EBankifyApplication.java
+++ src/main/java/com/bank/ebankify/EBankifyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EBankifyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EBankifyApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EBankifyApplication.class, args);
+	}
 
 }
