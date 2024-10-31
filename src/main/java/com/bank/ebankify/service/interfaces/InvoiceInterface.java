accd2c31dacb78c5b833a7540b8df02d2bc663d6
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-10-31 09:37:19 +0100

    adding methods to interfaces

--- src/main/java/com/bank/ebankify/service/interfaces/InvoiceInterface.java
+++ src/main/java/com/bank/ebankify/service/interfaces/InvoiceInterface.java
@@ -1,4 +1,19 @@
 package com.bank.ebankify.service.interfaces;
 
+import com.bank.ebankify.dto.InvoiceDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
 public interface InvoiceInterface {
+
+
+    InvoiceDto create(InvoiceDto invoiceDto);
+    Page<InvoiceDto> findAll(Pageable pageable);
+    Optional<InvoiceDto> findById(Long id);
+    InvoiceDto update(Long id, InvoiceDto invoiceDto);
+    void delete(Long id);
+    Page<InvoiceDto> findByStatus(String status, Pageable pageable);
+    Page<InvoiceDto> findByUserId(Long userId, Pageable pageable);
 }
--- src/main/java/com/bank/ebankify/service/interfaces/UserInterface.java
+++ src/main/java/com/bank/ebankify/service/interfaces/UserInterface.java
@@ -1,4 +1,17 @@
 package com.bank.ebankify.service.interfaces;
 
+import com.bank.ebankify.dto.UserDto;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
 public interface UserInterface {
+
+    UserDto create(UserDto userDto);
+    Page<UserDto> findAll(Pageable pageable);
+    Page<UserDto> findById(Long id);
+    UserDto update(Long id, UserDto userDto);
+    void delete(Long id);
+    Page<UserDto> findByRole(String role);
 }
